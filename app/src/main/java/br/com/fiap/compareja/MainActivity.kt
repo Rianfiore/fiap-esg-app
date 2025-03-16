0549e15a90acace5749e6e224c85f0b6463dd25e
Rian Fiore <rianfiore@gmail.com>
2025-03-16 19:29:51 -0300

    chore: create app screens folder

--- app/src/main/java/br/com/fiap/compareja/MainActivity.kt
+++ app/src/main/java/br/com/fiap/compareja/MainActivity.kt
@@ -6,42 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.fiap.compareja.ui.theme.CompareJaTheme
+import br.com.fiap.compareja.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CompareJaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CompareJaTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/br/com/fiap/compareja/screens/MarketStoreScreen.kt
@@ -0,0 +1,27 @@
+package br.com.fiap.compareja.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MarketStoreScreen() {
+//    Criar tela de Mercado aqui
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFFED145B))
+        .padding(32.dp)){
+        Text(text = "Mercado", fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color.White)
+    }
+}

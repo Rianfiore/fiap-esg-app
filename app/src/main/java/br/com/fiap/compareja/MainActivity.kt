a6b3ec41391567602a518499cec217b4745907ac
Rian Fiore <rianfiore@gmail.com>
2025-03-18 22:17:44 -0300

    feat: handle home screen

--- app/src/main/java/br/com/fiap/compareja/MainActivity.kt
+++ app/src/main/java/br/com/fiap/compareja/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import br.com.fiap.compareja.screens.map.MapScreen
 import br.com.fiap.compareja.ui.theme.CompareJaTheme
 import br.com.fiap.compareja.screens.market_store.MarketStoreScreen
 
--- app/src/main/java/br/com/fiap/compareja/screens/map/MapScreen.kt
+++ app/src/main/java/br/com/fiap/compareja/screens/map/MapScreen.kt
@@ -1,21 +1,17 @@
-package br.com.fiap.compareja.screens
+package br.com.fiap.compareja.screens.map
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
